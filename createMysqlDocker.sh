919d0e87f925a879ee4edd07cda77a132e6d0faa
AbelGuti <vegetalink333@gmail.com>
2016-11-24 17:09:10 -0600

    Update createMysqlDocker.sh

--- createMysqlDocker.sh
+++ createMysqlDocker.sh
@@ -4,10 +4,8 @@ NAME="$1"
 PSSWD="$2"
 PORT="$3"
 sudo docker run --name ${NAME} -e MYSQL_ROOT_PASSWORD=${PSSWD} -p ${PORT}:3306 -d mysql/mysql-server:5.5
-#sudo docker exec -it ${NAME} mysql -u root --password=${PSSWD} > script.sql
-#sudo docker exec -it ${NAME} /bin/bash mysql -u root --password=${PSSWD}
-#create database holi;
+
+#54.165.37.138 its my ip address, you must changed it by yours
 curl -L -X PUT http://54.165.37.138:5001/v2/keys/name -d value=${NAME}
 curl -L -X PUT http://54.165.37.138:5001/v2/keys/psswd -d value=${PSSWD}
 curl -L -X PUT http://54.165.37.138:5001/v2/keys/port -d value=${PORT}
-#curl -L  http://54.191.254.133:5005/v2/keys/message
